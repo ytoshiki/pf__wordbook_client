63797e9a373f4d69b21e5a5ac363ca911a258a4e
Toshiki Yoshioka <hnkc.ysoktsk@gmail.com>
2021-02-14 16:27:41 +0900

    test

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from '../reducers';
 import thunk from 'redux-thunk';
 
-export const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+export const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunk)));
